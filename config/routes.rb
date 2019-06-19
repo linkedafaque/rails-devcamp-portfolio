32c11b4e91d4a76585478afdb7b9d92f50ddfcfe
Afaque Ahmad <afaqueahmad@Afaques-MacBook-Pro.local>
2019-06-19 17:51:26 +0800

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # resources :portfolios
+
+  # Route all requests except 'show' to apis starting with portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+  # get 'pages/home'
+  # get 'pages/about'
+  # get 'pages/contact'
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  resources :blogs
+
+  root to: 'pages#home'
+end
