244c83bc5ea194b5348d402b2424baf6bd581f75
Afaque Ahmad <linkedafaque@gmail.com>
2019-06-21 11:44:08 +0800

    Added Topic model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_025555) do
+ActiveRecord::Schema.define(version: 2019_06_21_034103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 2019_06_20_025555) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utlitized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
