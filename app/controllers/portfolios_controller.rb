5ea270ef9f41eb5e251c4b32a0d60ff8deb4961f
linkedafaque <linkedafaque@gmail.com>
2019-06-21 15:59:54 +0800

    Display for Technology nested attributes
    1. Built nil objects in def new
    2. Put require in create to get technologies_attribute
    3. Get and put the 3 technologies in the @portfolio_item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,10 +7,11 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, technologies_attributes: [:name]))
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: "Your portfolio is now live!"}
