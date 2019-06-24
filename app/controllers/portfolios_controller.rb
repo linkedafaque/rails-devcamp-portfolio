922183888cf63f48438a5e4f16a019e68b73e997
linkedafaque <linkedafaque@gmail.com>
2019-06-24 12:47:10 +0800

    Added into a common method paras.require.permit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,7 +11,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: "Your portfolio is now live!"}
@@ -28,7 +28,7 @@ class PortfoliosController < ApplicationController
   def update
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: "Your portfolio is now updated!"}
       else
         format.html { render :new }
@@ -47,4 +47,12 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_url, notice: "Portfolio was removed!"}
     end
   end
+
+  private
+
+    def portfolio_params
+      params.require(:portfolio).permit(:title,
+                                        :subtitle,
+                                        technologies_attributes: [:name])
+    end
 end
